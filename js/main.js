9a0fd3b008b32bc7c86a149e8d2ab0f4f96ad555
Ldesir05 <ldesir5@mail.valenciacollege.edu>
2020-02-25 15:29:24 -0500

    Added social media icons

--- js/main.js
+++ js/main.js
@@ -160,4 +160,10 @@ jQuery(document).ready(function($) {
         }
     });
 
+//    Responsible for video popup
+    $('#headerVideoLink').magnificPopup({
+        type:'inline',
+        midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+    });
+
 });
